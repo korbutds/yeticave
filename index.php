68840570b8d2cb53e935c0d0b5770f844ea016ae
Dmitriy Korbut <d.korbut@vk.team>
2024-02-11 16:01:31 +0300

    Add lots

--- index.php
+++ index.php
@@ -12,6 +12,45 @@ $categories = [
   'other' => 'Разное',
 ];
 
+$lots_information = [
+  [
+    'title' => '2014 Rossignol District Snowboard',
+    'category' => 'boards',
+    'price' => 10999,
+    'image' => 'img/lot-1.jpg',
+  ],
+  [
+    'title' => 'DC Ply Mens 2016/2017 Snowboard',
+    'category' => 'boards',
+    'price' => 159999,
+    'image' => 'img/lot-2.jpg',
+  ],
+  [
+    'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+    'category' => 'attachment',
+    'price' => 8000,
+    'image' => 'img/lot-3.jpg',
+  ],
+  [
+    'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
+    'category' => 'boots',
+    'price' => 10999,
+    'image' => 'img/lot-4.jpg',
+  ],
+  [
+    'title' => 'Куртка для сноуборда DC Mutiny Charocal',
+    'category' => 'clothing',
+    'price' => 7500,
+    'image' => 'img/lot-5.jpg',
+  ],
+  [
+    'title' => 'Маска Oakley Canopy',
+    'category' => 'other',
+    'price' => 5400,
+    'image' => 'img/lot-6.jpg',
+  ],
+];
+
 ?>
 <!DOCTYPE html>
 <html lang="ru">
