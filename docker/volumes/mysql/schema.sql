91bdf1e70e4e29fd846f8885c331f2fb553d4b53
Dmitriy Korbut <d.korbut@vk.team>
2024-02-18 11:24:42 +0300

    Refactor database

--- docker/volumes/mysql/schema.sql
+++ docker/volumes/mysql/schema.sql
@@ -1,20 +1,49 @@
+DROP DATABASE IF EXISTS yeticave;
+
+CREATE DATABASE yeticave
+    DEFAULT CHARACTER SET utf8
+    DEFAULT COLLATE utf8_general_ci;
+
+USE yeticave;
+
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(128) NOT NULL UNIQUE,
-    password VARCHAR(128) NOT NULL
-);
-
-CREATE TABLE lots (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    title VARCHAR(128) NOT NULL,
-    category_id INT DEFAULT NULL,
-    price DECIMAL NOT NULL,
-    image VARCHAR(128) NOT NULL,
-    timer TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    password CHAR(12) NOT NULL,
+    date_registration DATETIME DEFAULT CURRENT_TIMESTAMP,
+    user_name VARCHAR(128),
+    contacts TEXT
 );
 
 CREATE TABLE categories (
    id INT AUTO_INCREMENT PRIMARY KEY,
-   category VARCHAR(128) NOT NULL UNIQUE
+   category VARCHAR(128) UNIQUE,
+   character_code VARCHAR(128) UNIQUE
 );
+
+CREATE TABLE lots (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    title VARCHAR(255),
+    lot_description TEXT,
+    image VARCHAR(255),
+    start_price INT,
+    date_finish DATE,
+    step INT,
+    user_id INT,
+    winner_id INT,
+    category_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (winner_id) REFERENCES users(id),
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+CREATE TABLE bets (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    date_bet DATETIME DEFAULT CURRENT_TIMESTAMP,
+    price_bet INT,
+    user_id INT,
+    lot_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (lot_id) REFERENCES lots(id)
+)
